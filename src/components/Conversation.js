c68a4968b1d78dbb5157c849fd4dbf8a7f6b509a
alexisrobin <alexisrobin.bonjour@gmail.com>
2018-10-09 00:31:35 +0200

    :lipstick: Updating Conversation view (auto scroll to latest message)

--- src/components/Conversation.js
+++ src/components/Conversation.js
@@ -1,34 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Conversation.scss';
 import { Media } from 'reactstrap';
 import Message from './Message';
 
+/**
+ * Message's list from a user to others.
+ */
+class Conversation extends Component {
 
-const Conversation = ({ messages, users }) => {
-    const currentUser = 1;
-    // Sorting messages by timestamp
-    messages = messages.sort(function(a,b){
-        return new Date(a.timestamp) - new Date(b.timestamp);
-    });
-    return (
-        <div className="conversation">
-            <Media list className="p-0">
-                {messages.map(message => {
-                    return (
-                        <Media tag="li"
-                            key={message.id}
-                            className={message.senderId === currentUser ? "justify-content-end" : "justify-content-start"}>
-                            <div>
-                                <Message
-                                    message={message}
-                                    senderName={users.filter(user => user.id === message.senderId).map(user => user.name)} />
-                            </div>
-                        </Media>
-                    )
-                })}
-            </Media>
-        </div>
-    )
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentUser: 1
+        }
+
+        // Sort messages by timestamp
+        this.messages = this.props.messages.sort(function (a, b) {
+            return new Date(a.timestamp) - new Date(b.timestamp);
+        });
+    }
+
+    /**
+     * Scroll to conversation latest message.
+     */
+    scrollToLatestMessage = () => {
+        this.conversationBottom.scrollIntoView();
+    }
+
+    componentDidMount() {
+        this.scrollToLatestMessage();
+    }
+
+    componentDidUpdate() {
+        this.scrollToLatestMessage();
+    }
+
+    render() {
+        return (
+            <div className="conversation">
+                <Media list className="p-0">
+                    {this.messages.map((message) => {
+                        return (
+                            <Media tag="li"
+                                key={message.id}
+                                className={message.senderId === this.state.currentUser ? "justify-content-end" : "justify-content-start"}>
+                                <div>
+                                    <Message
+                                        message={message}
+                                        senderName={this.props.users.filter(user => user.id === message.senderId).map(user => user.name)} />
+                                </div>
+                            </Media>
+                        )
+                    })}
+                </Media>
+                <div style={{ float: "left", clear: "both" }}
+                    ref={(el) => { this.conversationBottom = el; }}>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Conversation;
