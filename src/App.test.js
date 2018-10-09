b9056b2ce46148d1341e45f66146516b0b64f36b
alexisrobin <alexisrobin.bonjour@gmail.com>
2018-10-09 10:11:38 +0200

    :heavy_plus_sign: Adding dependencies for testing (chai, enzyme, sinon)

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { expect } from 'chai'
+import { shallow } from 'enzyme'
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('<App />', () => {
+  it('should renders without crashing', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('.App')).to.have.length(1);
+  })
+})
