9b4d4b1eb831af4e4b519f9a8e85a754693bba69
alexisrobin <alexisrobin.bonjour@gmail.com>
2018-10-09 15:52:22 +0200

    :heavy_plus_sign: Adding dependency for store's state logging (redux-logger)

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,4 +1,6 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
 import rootReducer from './reducers';
 
-export const configureStore = () => createStore(rootReducer);
+
+export const configureStore = () => createStore(rootReducer, applyMiddleware(logger));
