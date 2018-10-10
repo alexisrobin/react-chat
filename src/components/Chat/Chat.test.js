4bf456ac95149d9ac4c9d0bf1445c302fa68bcf8
alexisrobin <alexisrobin.bonjour@gmail.com>
2018-10-10 22:32:02 +0200

    :white_check_mark: Adding tests for Chat component

--- src/components/Chat/Chat.test.js
+++ src/components/Chat/Chat.test.js
@@ -5,15 +5,29 @@ import Chat from './Chat';
 import { messages } from '../../constants/messages';
 import { users } from '../../constants/users';
 
+const currentUserId = 1;
+
 describe('<Chat />', () => {
     it('should match its reference snapshot', () => {
         const wrapper = shallow(
             <Chat 
                 messages={messages}
                 users={users}
-                currentUserId={1}/>
+                currentUserId={currentUserId}/>
         );
 
         expect(wrapper).to.matchSnapshot()
+    })
+
+    it('should display current user name as header', () => {
+        const wrapper = shallow(
+            <Chat 
+                messages={messages}
+                users={users}
+                currentUserId={currentUserId}/>
+        );
+
+        const currentUserName = users.filter(user => user.id === currentUserId).shift().name;
+        expect(wrapper.find(".chat-header").contains(currentUserName)).to.be.true();
     })
 })
